7be2ffaf813b8d8144c792799e0181f1328472c5
ddwee <103949785+ddwee@users.noreply.github.com>
2025-06-14 01:12:40 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ export default defineConfig({
         plugins: [yaml()],
     },
     prefetch: true,
-    site,
+    site 'https://ddwee.github.io',
     scopedStyleStrategy: 'class',
     trailingSlash: 'always',
     build: {
