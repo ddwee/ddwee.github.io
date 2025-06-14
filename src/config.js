547abf71810d52f1a5783a46a3ac9e384ce52140
ddwee <103949785+ddwee@users.noreply.github.com>
2025-06-14 17:59:38 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,15 +2,15 @@ export const PandaConfig = {
     title: 'Panda Blog',
     description: 'Panda Blog, a blog powered by Astro',
     start: '2016',
-    site: 'https://astro-theme-panda.vercel.app/',
-    defaultLocale: 'en',
+    site: 'https://ddwee.github.io',
+    defaultLocale: 'ja',
     navbar: [
         // {title: 'Posts', url: '/'}, // auto generated
         { title: 'about', url: '/about/' }
     ],
     footer: [
         { title: 'rss', url: '/rss.xml/' },
-        { title: 'contact', url: 'https://github.com/yuhangch/astro-theme-panda/issues/new' },
-        { title: 'github', url: 'https://github.com/yuhangch/astro-theme-panda' }
+        { title: 'contact', url: 'https://github.com/ddwee/ddwee.github.io/issues/new' },
+        { title: 'github', url: 'https://github.com/ddwee/ddwee.github.io' }
     ]
 }
