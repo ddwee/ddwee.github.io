ad5c6f5fe2a09c4d420beea468ef5bc4792f7fda
ddwee <103949785+ddwee@users.noreply.github.com>
2025-06-14 21:17:52 +0900

    Update format.ts

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -2,8 +2,8 @@ import { DateTime } from 'luxon'
 import { PandaConfig } from '../config.js'
 const { defaultLocale } = PandaConfig
 
-export function relativeTo(dateStr: string, locale = 'zh') {
-    locale ??= 'zh'
+export function relativeTo(dateStr: string, locale = 'ja') {
+    locale ??= 'ja'
     locale = defaultLocale // force to default language
     return DateTime.fromISO(dateStr).toRelative({
         base: DateTime.now(),
@@ -11,7 +11,7 @@ export function relativeTo(dateStr: string, locale = 'zh') {
     })
 }
 
-export function formatDateMD(dateStr: string, locale: string = 'zh') {
+export function formatDateMD(dateStr: string, locale: string = 'ja') {
     const date = DateTime.fromISO(dateStr)
     locale = defaultLocale // force to default language
     if (locale === 'en') {
@@ -20,7 +20,7 @@ export function formatDateMD(dateStr: string, locale: string = 'zh') {
     return date.toFormat('MM-dd')
 }
 
-export function formatDateYMD(dateString: string, locale: string = 'zh') {
+export function formatDateYMD(dateString: string, locale: string = 'ja') {
     locale = defaultLocale // force to default language
     const date = DateTime.fromISO(dateString)
     if (locale === 'en') {
