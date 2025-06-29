4c86ba15fe268bd1caf475ac095c0599b5eba841
ddwee <103949785+ddwee@users.noreply.github.com>
2025-06-29 23:32:07 +0900

    Update format.ts

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -15,18 +15,18 @@ export function formatDateMD(dateStr: string, locale: string = 'ja') {
     const date = DateTime.fromISO(dateStr)
     locale = defaultLocale // force to default language
     if (locale === 'en') {
-        return date.setLocale(locale).toFormat('dd, MMM')
+        return date.setLocale(locale).toFormat('d, MMM')
     }
-    return date.toFormat('MM-dd')
+    return date.toFormat('M月d日')
 }
 
 export function formatDateYMD(dateString: string, locale: string = 'ja') {
     locale = defaultLocale // force to default language
     const date = DateTime.fromISO(dateString)
     if (locale === 'en') {
-        return date.setLocale(locale).toFormat('dd, MMM, yyyy')
+        return date.setLocale(locale).toFormat('d, MMM, yyyy')
     }
-    return date.toFormat('yyyy-MM-dd')
+    return date.toFormat('yyyy年M月d日')
 }
 
 export function slugifySpace(old: string) {
